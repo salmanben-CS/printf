8777098841eca81312c4042ce2e90f95bc0aa853
salmanben-CS <salmanbenomar250@gmail.com>
2023-04-24 16:09:56 -0700

    fix error

--- _printf.c
+++ _printf.c
@@ -1,46 +1,42 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
-#include "main.h"
-/**
- *  _printf - print anything
- * @format :array of characters(string) contains the formats of elements  printed
- *  Return:the number of characters printed
- */
+
 int _printf(const char *format, ...)
 {
 int count = 0;
-char c;
+const char *c;
 va_list args;
 va_start(args, format);
+
 if (format == NULL)
-return (-1);
-while ((c = *format) != '\0')
 {
-if (c != '%')
+return -1;
+}
+
+for (c = format; *c != '\0'; c++)
 {
-write(STDOUT_FILENO, &c, 1);
+if (*c != '%')
+{
+write(STDOUT_FILENO, c, 1);
 count++;
 }
 else
 {
-format++;
-c = *format;
-switch (c)
+c++;
+switch (*c)
 {
 case '%':
- {
 write(STDOUT_FILENO, "%", 1);
 count++;
-}
 break;
 case 'c':
 {
-char *arg = va_arg(args, char*);
-write(STDOUT_FILENO, arg, 1);
+char arg = va_arg(args, int);
+write(STDOUT_FILENO, &arg, 1);
 count++;
-}
 break;
+}
 case 's':
 {
 char *str = va_arg(args, char*);
@@ -50,13 +46,16 @@ write(STDOUT_FILENO, str, 1);
 str++;
 count++;
 }
-}
 break;
 }
+default:
+write(STDOUT_FILENO, "Error: Invalid format specifier\n", 32);
+return -1;
 }
-format++;
 }
+}
+
 va_end(args);
-return (count);
+return count;
 }
 
